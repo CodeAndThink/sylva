0a92e9df2699be3f4b854dd5a4658ee8a9d7f63d
truongnq <truongnq@newwave.com.vn>
2026-06-23 18:01:33 +0700

    feat: update application launcher icons and integrate into splash screen UI

--- lib/presentation/features/splash/splash_page.dart
+++ lib/presentation/features/splash/splash_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:sylva/core/constants/app_assets.dart';
 import 'package:sylva/core/di/injection.dart';
 import 'package:sylva/presentation/app/app_cubit.dart';
 import 'package:sylva/presentation/features/splash/splash_cubit.dart';
 import 'package:sylva/presentation/features/splash/splash_navigator.dart';
+import 'package:sylva/presentation/widgets/images/app_asset_image.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -40,8 +42,22 @@ class __SplashChildPageState extends State<_SplashChildPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SpinKitRipple(color: Theme.of(context).colorScheme.primary),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          SpinKitRipple(
+            color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.5),
+            size: 200,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(60),
+            child: AppAssetImage(
+              path: AppAssets.icAppIcon,
+              width: 120,
+              height: 120,
+            ),
+          ),
+        ],
       ),
     );
   }

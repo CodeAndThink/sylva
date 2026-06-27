bf33f41a3c800c781c615a2a61f40f9a97f5da83
truongnq <trg1432001.tu@gmail.com>
2026-06-27 16:30:21 +0700

    feat: add AppConfigs class and bump build version to 0.1.3+4

--- lib/core/configs/app_configs.dart
+++ lib/core/configs/app_configs.dart
@@ -2,5 +2,5 @@ class AppConfigs {
   static const String appName = "Sylva";
   static const String packageName = "com.truongngo.sylva";
   static const String version = "0.1.3";
-  static const String buildNumber = "3";
+  static const String buildNumber = "4";
 }

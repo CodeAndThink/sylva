79d6197c847fd014b9f15e01d7e400ad90f6a235
truongnq <trg1432001.tu@gmail.com>
2026-06-27 17:16:43 +0700

    feat: initialize app configuration constants and update project version to 0.2.0+5

--- lib/core/configs/app_configs.dart
+++ lib/core/configs/app_configs.dart
@@ -1,6 +1,6 @@
 class AppConfigs {
   static const String appName = "Sylva";
   static const String packageName = "com.truongngo.sylva";
-  static const String version = "0.1.3";
-  static const String buildNumber = "4";
+  static const String version = "0.2.0";
+  static const String buildNumber = "5";
 }

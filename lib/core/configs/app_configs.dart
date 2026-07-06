47c90c4c05478f2a499f5e368b3f8a60e70a5b7e
Dave <datdt@newwave.com.vn>
2026-07-06 16:03:41 +0700

    chore: bump app version to 0.3.2+9

--- lib/core/configs/app_configs.dart
+++ lib/core/configs/app_configs.dart
@@ -1,6 +1,6 @@
 class AppConfigs {
   static const String appName = "Sylva";
   static const String packageName = "com.truongngo.sylva";
-  static const String version = "0.3.1";
-  static const String buildNumber = "8";
+  static const String version = "0.3.2";
+  static const String buildNumber = "9";
 }

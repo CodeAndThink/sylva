8b318eaedca684ca1718ce6a22aa9aa9a92e7921
truongnq <trg1432001.tu@gmail.com>
2026-07-11 21:48:40 +0700

    feat: implement sponsors and settings pages with UI components and state management

--- lib/presentation/features/settings/settings_page.dart
+++ lib/presentation/features/settings/settings_page.dart
@@ -166,12 +166,15 @@ class __SettingsChildPageState extends State<_SettingsChildPage> {
                         state.seedColor.toARGB32() == color.toARGB32();
                     return SizedBox(
                       height: 38,
-                      child: ThemeColorButton(
-                        color: color,
-                        isSelected: isSelected,
-                        onTap: () {
-                          _themeCubit.updateSeedColor(color: color);
-                        },
+                      width: 38,
+                      child: Center(
+                        child: ThemeColorButton(
+                          color: color,
+                          isSelected: isSelected,
+                          onTap: () {
+                            _themeCubit.updateSeedColor(color: color);
+                          },
+                        ),
                       ),
                     );
                   }),

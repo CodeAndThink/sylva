0aadc4bb5251795fbbcb069a79ac1b18a6dda01d
Truong Ngo <87740125+CodeAndThink@users.noreply.github.com>
2026-07-11 11:34:20 +0700

    feat: implement ShareImagePreview widget and add file_picker dependency (#41)

--- lib/presentation/features/home/home_cubit.dart
+++ lib/presentation/features/home/home_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:sylva/presentation/features/home/home_navigator.dart';
 import 'package:sylva/presentation/features/home/home_state.dart';
 import 'package:sylva/presentation/widgets/cubit/base_cubit.dart';
@@ -11,13 +11,12 @@ class HomeCubit extends BaseCubit<HomeState> {
 
   Future<void> pickImageFromGallery() async {
     try {
-      final picker = ImagePicker();
-      final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-      if (image != null) {
-        navigator.goToPhotoPreview(imagePath: image.path);
+      PlatformFile? file = await FilePicker.pickFile(type: FileType.image);
+      if (file != null && file.path != null) {
+        navigator.goToPhotoPreview(imagePath: file.path!);
       }
     } catch (e) {
-      debugPrint('Error picking image: $e');
+      debugPrint('Error picking file: $e');
     }
   }
 }

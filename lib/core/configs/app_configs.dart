7a6ce7be9c0fc89d1bb5382d2200502f408503b6
“truongnq” <trg1432001.tu@gmail.com>
2026-07-16 13:54:22 +0700

    chore: bump build number to 18

--- lib/core/configs/app_configs.dart
+++ lib/core/configs/app_configs.dart
@@ -2,5 +2,5 @@ class AppConfigs {
   static const String appName = "Sylva";
   static const String packageName = "com.truongngo.sylva";
   static const String version = "0.3.10";
-  static const String buildNumber = "17";
+  static const String buildNumber = "18";
 }

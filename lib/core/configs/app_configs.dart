f09772f0e5da3276b0e9fa5fe201a1e6ecb8f2b7
truongnq <trg1432001.tu@gmail.com>
2026-07-16 21:58:16 +0700

    chore: pump project version to 0.4.1+20

--- lib/core/configs/app_configs.dart
+++ lib/core/configs/app_configs.dart
@@ -1,6 +1,6 @@
 class AppConfigs {
   static const String appName = "Sylva";
   static const String packageName = "com.truongngo.sylva";
-  static const String version = "0.4.0";
-  static const String buildNumber = "19";
+  static const String version = "0.4.1";
+  static const String buildNumber = "20";
 }

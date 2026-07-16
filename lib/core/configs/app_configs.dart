b949e755d45b9a593acff578fc9f86448551c0c4
“truongnq” <trg1432001.tu@gmail.com>
2026-07-16 15:51:37 +0700

    chore: bump app version to 0.4.0+19

--- lib/core/configs/app_configs.dart
+++ lib/core/configs/app_configs.dart
@@ -1,6 +1,6 @@
 class AppConfigs {
   static const String appName = "Sylva";
   static const String packageName = "com.truongngo.sylva";
-  static const String version = "0.3.10";
-  static const String buildNumber = "18";
+  static const String version = "0.4.0";
+  static const String buildNumber = "19";
 }
